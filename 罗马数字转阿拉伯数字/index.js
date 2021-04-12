9d4759e906dfba243a2283c0e2feeece0c914c49
liusijie02 <liusijie02@meituan.com>
2021-04-13 01:40:17 +0800

    todo

--- 罗马数字转阿拉伯数字/index.js
+++ 罗马数字转阿拉伯数字/index.js
@@ -1,21 +1,21 @@
 const romanToInt = s => {
-    const map = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
-    }
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
 
-    let n = 0
-    for(let i = 0, len=s.length; i < len; i++){
-        map[s[i]] < map[s[i+1]] ? n -= map[s[i]] : n += map[s[i]]
-    }
+  let n = 0
+  for(let i = 0, len=s.length; i < len; i++){
+    map[s[i]] < map[s[i+1]] ? n -= map[s[i]] : n += map[s[i]]
+  }
 
-    return n
-};
+  return n
+}
 
 const a = romanToInt('MCDLXXVI')
 console.log(a, '-----a-----')
