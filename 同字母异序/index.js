204b23fc6b501e6b564a8be6ba6cb3ea1ae470bf
liusijie02 <liusijie02@meituan.com>
2021-04-15 03:02:34 +0800

    todo

--- 同字母异序/index.js
+++ 同字母异序/index.js
@@ -1,3 +1,13 @@
 // isAnagram("anagram", "nagaram") // => return true. 
 // isAnagram("rat", "car") // => return false.
 
+const isAnagram = (a, b) => {
+  if (a.length !== b.length) return false
+  if (a === b) return true
+  const c = a.split('').sort().join('')
+  const d = b.split('').sort().join('')
+  return c === d
+}
+
+console.log(isAnagram("anagram", "nagaram"), '-----1-----')
+console.log(isAnagram("rat", "car"), '-----2-----')
